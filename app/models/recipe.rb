96db6fa15197e1c286fac02fc47421accd79cb37
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-19 18:34:56 +0000

    Update Recipe model validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,19 @@
 class Recipe < ApplicationRecord
+  belongs_to :creator, class_name: :User, foreign_key: "creator_id"
+  belongs_to :poster, class_name: :User, foreign_key: "poster_id"
   has_many :quantities_and_ingredients, dependent: :destroy
+  has_many :comments
+  has_many :users, through: :comments
+  has_many :ratings
+  has_many :users, through: :ratings
+  has_many :saved_recipes
+  has_many :users, through: :saved_recipesecipes
+
 
   validates :name, presence: true
   validates :category, presence: true, inclusion: { in: ["Entrée / Apéro", "Plat principal végétarien", "Plat principal à base de viande", "Plat principal à base de poisson", "Dessert"] }
   validates :cooktime, inclusion: { in: [0, 5, 10, 15, 20, 25, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 240, 300, 360, 420] }
   validates :preptime, inclusion: { in: [0, 5, 10, 15, 20, 25, 30, 45, 60, 75, 90] }
+  validates :creator_id, presence: true
+  validates :poster_id, presence: true
 end
