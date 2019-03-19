6231619a26449558bb13dd8241c658e1028b7f46
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-19 19:39:46 +0000

    Update seed file to create Recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 require 'faker'
 
 p "Start seeding..."
+
+Recipe.destroy_all
+p "All recipes deleted"
 User.destroy_all
 p "All users deleted"
 
@@ -22,6 +25,20 @@ p "Creating 25 fake users"
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: 123456
+  )
+  print "."
+end
+
+p "Creating 40 recipes"
+40.times do
+  Recipe.create!(
+    name: Faker::Food.dish,
+    cooktime: [0, 5, 10, 15, 20, 25, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 240, 300, 360, 420].sample,
+    preptime: [0, 5, 10, 15, 20, 25, 30, 45, 60, 75, 90].sample,
+    category: ["Entrée / Apéro", "Plat principal végétarien", "Plat principal à base de viande", "Plat principal à base de poisson", "Dessert"].sample,
+    creator_id: rand(User.first.id..User.last.id),
+    poster_id: rand(User.first.id..User.last.id),
+    description: Faker::Food.description
   )
   print "."
 end
