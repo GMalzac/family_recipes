d5dcce9cebcd02126c52c024ac1ed9499bfa7758
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-22 17:15:00 +0000

    Fix recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :creator, class_name: :User, foreign_key: "creator_id"
   belongs_to :poster, class_name: :User, foreign_key: "poster_id"
-  has_many :quantities_and_ingredients
+  has_many :quantity_ingredients
   has_many :comments
   has_many :users, through: :comments
   has_many :ratings
