ba6c970371cd820d1ba62932df17f1516b6b9e96
Gregoire Malzac <gregoire.malzac@gmail.com>
2019-03-22 18:00:40 +0000

    Update page home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,12 @@ class PagesController < ApplicationController
 
   def home
     @latest_recipes = Recipe.order(created_at: :desc).limit(3)
+
     @random_starter = Recipe.where(category: "Entrée / Apéro").limit(1).order("RANDOM()").first
+
     random_main_category = ["Plat principal végétarien", "Plat principal à base de viande", "Plat principal à base de poisson"].sample
     @random_main = Recipe.where(category: random_main_category).limit(1).order("RANDOM()").first
+
     @random_desert = Recipe.where(category: "Dessert").limit(1).order("RANDOM()").first
   end
 end
